5cb7766be943014dd519852b85c3b20997700cd4
Abhishek <abhishekmohapatra768@gmail.com>
2026-05-15 15:35:38 +0530

    update project gradle to maven

--- SpringBootMS-Proj16-AdminServer-MiniProject/src/test/java/com/spring/SpringBootMsProj16AdminServerMiniProjectApplicationTests.java
+++ SpringBootMS-Proj16-AdminServer-MiniProject/src/test/java/com/spring/SpringBootMsProj16AdminServerMiniProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootMsProj16ConfigServerMiniProjectApplicationTests {
+class SpringBootMsProj16AdminServerMiniProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
